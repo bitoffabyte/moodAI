d23ffc8da98fe036d893cd0ddc3c182f95cf61dc
R Narayan <nr.rnarayan@gmail.com>
2021-03-28 14:37:01 +0530

    Added new empty dummy firebase credentials :sparkles:

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
 var firebaseConfig = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP__MESSAGING_ID,
-	appId: process.env.REACT_APP_APP_ID,
+	apiKey: 'AIzaSyA75EhCApu55-_Ov33zjD3LXCKI8PWQUCs',
+	authDomain: 'face-20bb0.firebaseapp.com',
+	databaseURL: 'https://face-20bb0-default-rtdb.firebaseio.com',
+	projectId: 'face-20bb0',
+	storageBucket: 'face-20bb0.appspot.com',
+	messagingSenderId: '1099329693539',
+	appId: '1:1099329693539:web:f318f33aa6891df25e5556',
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
