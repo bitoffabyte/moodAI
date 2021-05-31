8d0299bf060daa084232064ebd050d55f7b9b3ad
R Narayan <nr.rnarayan@gmail.com>
2021-05-31 10:09:18 +0530

    asd

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 var firebaseConfig = {
-	apiKey: 'AIzaSyBANCB7y0ApW-_iN1VsLe_MYU-q7EebpQ8',
-	authDomain: 'face-3a6e6.firebaseapp.com',
-	databaseURL: 'https://face-3a6e6-default-rtdb.firebaseio.com',
-	projectId: 'face-3a6e6',
-	storageBucket: 'face-3a6e6.appspot.com',
-	messagingSenderId: '133859590463',
-	appId: '1:133859590463:web:cd1cc1a4ee60f643fb538a',
+	apiKey: 'AIzaSyAb2hr1b_q4I96QfQuyn2AsmcmxxHvDFBY',
+	authDomain: 'dfzhbdfz.firebaseapp.com',
+	databaseURL: 'https://dfzhbdfz-default-rtdb.firebaseio.com',
+	projectId: 'dfzhbdfz',
+	storageBucket: 'dfzhbdfz.appspot.com',
+	messagingSenderId: '776797359347',
+	appId: '1:776797359347:web:3cecce9bf3e9bbc3a43b86',
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
